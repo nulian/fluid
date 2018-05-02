2343b5b7895ee8fc14d8a41cc5ccc44641768660
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-02 18:04:03 -0400

    fix: infinite loop when parsing variable

--- lib/liquid/nimble_parser.ex
+++ lib/liquid/nimble_parser.ex
@@ -10,27 +10,18 @@ defmodule Liquid.NimbleParser do
   @space 0x0020
   @start_tag "{%"
   @end_tag "%}"
-  @start_var "{{"
-  @end_var "}}"
+  @start_variable "{{"
+  @end_variable "}}"
   @point 0x002E
   @question_mark 0x003F
   @underscore 0x005F
 
-  ###################################### General use combinators  #######################################
-
-  # All utf8 valid characters or empty limited by start/end of tag/variable
-  # Name :: /[_A-Za-z][_0-9A-Za-z]*/
+  ############################### General use combinators  ############################
 
   # Horizontal Tab (U+0009) + Space (U+0020)
-
-  whitespace =
-    ascii_char([
-      @horizontal_tab,
-      @space
-    ])
+  whitespace = ascii_char([@space, @horizontal_tab])
 
   # Remove all :whitespace
-
   ignore_whitespaces =
     whitespace
     |> repeat()
@@ -40,87 +31,104 @@ defmodule Liquid.NimbleParser do
     empty()
     |> concat(ignore_whitespaces)
     |> ascii_char([@underscore, ?A..?Z, ?a..?z])
-    |> times(ascii_char([@point, @underscore, @question_mark, ?0..?9, ?A..?Z, ?a..?z]), min: 1)
+    |> optional(repeat(ascii_char([
+            @point, @underscore, @question_mark, ?0..?9, ?A..?Z, ?a..?z]
+          )))
     |> concat(ignore_whitespaces)
     |> reduce({List, :to_string, []})
     |> tag(:variable_name)
 
-  literal =
+  defparsec(:variable_name, variable_name)
+
+  start_variable =
     empty()
-    |> repeat_until(utf8_char([]), [
-      string(@start_var),
-      string(@end_var),
-      string(@start_tag),
-      string(@end_tag)
-    ])
-    |> reduce({List, :to_string, []})
-    |> tag(:literal)
+    |> concat(string(@start_variable))
+    |> concat(ignore_whitespaces)
+    |> ignore()
+
+  end_variable =
+    ignore_whitespaces
+    |> concat(string(@end_variable))
+    |> ignore()
 
   liquid_variable =
-    empty()
-    |> string(@start_var)
-    |> concat(variable_name)
-    |> concat(string(@end_var))
+    start_variable
+    |> concat(parsec(:variable_name))
+    |> concat(end_variable)
     |> tag(:variable)
+    |> optional(parsec(:__parse__))
 
-  ###################################### General use combinators  #######################################
+  defparsec(:liquid_variable, liquid_variable)
 
-  ######################################        Tags              #######################################
-  # |> concat(value)
+  start_tag =
+    empty()
+    |> string(@start_tag)
+    |> concat(ignore_whitespaces)
+    |> ignore()
 
-  liquid_tag =
-    choice([
-      empty(),
-      empty()
-    ])
+  defparsecp(:start_tag, start_tag)
 
-  assign_tag =
-    string("assign")
+  end_tag =
+    ignore_whitespaces
+    |> concat(string(@end_tag))
+    |> ignore()
+
+  defparsecp(:end_tag, end_tag)
+  ############################### General use combinators  ############################
+
+  ################################        Tags              ###########################
+
+  assign =
+    empty()
+    |> parsec(:start_tag)
+    |> concat(string("assign"))
     |> ignore()
     |> concat(variable_name)
-    |> ignore(string("="))
+    |> concat(ignore(string("=")))
+    # |> concat(value)
+    |> concat(parsec(:end_tag))
     |> tag(:assign)
+    |> optional(parsec(:__parse__))
 
   decrement =
-    string("decrement")
+    empty()
+    |> parsec(:start_tag)
+    |> string("decrement")
     |> concat(variable_name)
+    |> concat(parsec(:end_tag))
+    |> tag(:decrement)
+    |> optional(parsec(:__parse__))
 
-  #   star_for =   # {% for var in vars %}
-  #     start_tag #{%
-  #     |> concat(string("for"))
-  #     |> concat(end_tag)
-  #     |> literal
-  #
-  #
-  # close_for =
-  #   start_tag
-  #   |> concat(string("endfor"))
-  #   |> concat(end_tag)
-  #
-  #
-  #   for_tag =
-  #       start_for
-  #       |> concat(choice([
-  #         literal,
-  #         tags...
-  #         ])
-  #       |> concat(end_for)
+  increment =
+    empty()
+    |> parsec(:start_tag)
+    |> string("increment")
+    |> concat(variable_name)
+    |> concat(parsec(:end_tag))
+    |> tag(:increment)
+    |> optional(parsec(:__parse__))
 
-  # {%for %} {%for%} {%endfor%}{% endfor %} parse
+  defparsec(:liquid_tag,
+    choice([
+      assign,
+      decrement,
+      increment
+    ])
+  )
 
-  ######################################        end Tags              #######################################
+  ################################ end Tags #######################################
 
-  ############################################# Lexical Tokens ##########################################
+  ################################## Lexical Tokens ###############################
 
-  # # Token ::
-  # #   - Punctuator
-  # #   - IntValue
-  # #   - FloatValue
-  # #   - StringValue
+  # Token ::
+  #   - Punctuator
+  #   - IntValue
+  #   - FloatValue
+  #   - StringValue
 
-  # # Punctuator :: one of ! $ ( ) ... : = @ [ ] { | }
-  # # Note: No `punctuator` combinator(s) defined; these characters are matched
-  # #       explicitly with `ascii_char/1` in other combinators.
+  # Punctuator :: one of ! $ ( ) ... : = @ [ ] { | }
+  # Note: No `punctuator` combinator(s) defined; these characters are matched
+  #       explicitly with `ascii_char/1` in other combinators.
 
   # NegativeSign :: -
   negative_sign = ascii_char([?-])
@@ -148,15 +156,15 @@ defmodule Liquid.NimbleParser do
     |> concat(integer_part)
     |> traverse({:build_int_value, []})
 
-  def build_int_value(rest, value, context, line, offset) do
+  defp build_int_value(rest, value, context, line, offset) do
     do_build_int_value(rest, Enum.reverse(value), context, line, offset)
   end
 
-  def do_build_int_value(_rest, [?- | digits], context, _, _) do
+  defp do_build_int_value(_rest, [?- | digits], context, _, _) do
     {[List.to_integer(digits) * -1], context}
   end
 
-  def do_build_int_value(_rest, digits, context, _, _) do
+  defp do_build_int_value(_rest, digits, context, _, _) do
     {[List.to_integer(digits)], context}
   end
 
@@ -209,15 +217,24 @@ defmodule Liquid.NimbleParser do
   # NullValue : `nil`
   null_value = string("nil")
 
+  # Value[Const] :
+  #   - IntValue
+  #   - FloatValue
+  #   - StringValue
+  #   - BooleanValue
+  #   - NullValue
+  #   - ListValue[?Const]
   value =
     choice([
-      int_value,
       float_value,
+      int_value,
       string_value,
       boolean_value,
       null_value,
       parsec(:list_value)
     ])
+
+  defparsec(:value, value)
 
   # ListValue[Const] :
   #   - [ ]
@@ -227,55 +244,45 @@ defmodule Liquid.NimbleParser do
       ascii_char([?[])
       |> ascii_char([?]]),
       ascii_char([?[])
-      |> times(value, min: 1)
+      |> times(parsec(:value), min: 1)
       |> ascii_char([?]])
     ])
 
-  # Value[Const] :
-  #   - IntValue
-  #   - FloatValue
-  #   - StringValue
-  #   - BooleanValue
-  #   - NullValue
-  #   - ListValue[?Const]
+  defparsec(:list_value, list_value)
 
-  ############################################# End lexical Tokens ##########################################
+  #################################### End lexical Tokens #####################################
 
-  ############################################### Parser ##########################################
+  ########################################### Parser ##########################################
+
+  # All utf8 valid characters or empty limited by start/end of tag/variable
+  # Name :: /[_A-Za-z][_0-9A-Za-z]*/
+  literal =
+    empty()
+    |> repeat_until(utf8_char([]), [
+        string(@start_variable),
+        string(@end_variable),
+        string(@start_tag),
+        string(@end_tag)
+      ])
+    |> reduce({List, :to_string, []})
+    |> tag(:literal)
+
+  defparsec(:__parse__,
+    literal
+    |> optional(choice([parsec(:liquid_tag), parsec(:liquid_variable)])))
+
   @doc """
   Valid and parse liquid markup.
   """
   @spec parse(String.t()) :: {:ok | :error, any()}
+  def parse(""), do: {:ok, ""}
   def parse(markup) do
     case __parse__(markup) do
-      {:ok, [template], "", _, _, _} ->
+      {:ok, template, "", _, _, _} ->
         {:ok, template}
 
-      {:error, err, _, _, _, _} ->
-        {:error, err}
+      {:ok, _, rest, _, _, _} ->
+        {:error, "Error parsing: #{rest}"}
     end
   end
-
-  definition =
-    choice([
-      parsec(:__expression__),
-      parsec(:__literal__)
-    ])
-
-  defparsec(:__parse__, repeat_while(definition, {:not_eof, []}))
-
-  defparsecp(:__literal__, literal)
-
-  defparsecp(
-    :__expression__,
-    choice([
-      liquid_tag,
-      liquid_variable
-    ])
-  )
-
-  defp not_eof("", context, _, _), do: {:halt, context}
-  defp not_eof(_, context, _, _), do: {:cont, context}
-
-  ############################################# End  Parser #############################################
 end
