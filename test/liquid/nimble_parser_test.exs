f5261928a26c3bbf331261ac55072c709a77b6a8
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-07 05:56:47 -0400

    test: add complex assertion to string value test

--- test/liquid/nimble_parser_test.exs
+++ test/liquid/nimble_parser_test.exs
@@ -23,11 +23,11 @@ defmodule Liquid.NimbleParserTest do
   test "string value" do
     test_combinator(~S("abc"), &Parser.value/1, [value: "abc"])
     test_combinator(~S(""), &Parser.value/1, [value: ""])
+    test_combinator(~S("mom's chicken"), &Parser.value/1, [value: "mom's chicken"])
+    test_combinator(~S("這是傳統的中文"), &Parser.value/1, [value: "這是傳統的中文"])
+    test_combinator(~S( "هذا باللغة العربية"), &Parser.value/1, [value: "هذا باللغة العربية"])
+    test_combinator(~S("😁😂😃😉"), &Parser.value/1, [value: "😁😂😃😉"])
   end
-
-  #   - BooleanValue
-  #   - NullValue
-  #   - ListValue[?Const]
 
   test "boolean values" do
     test_combinator("true", &Parser.value/1, [value: "true"])
