c2fc4cd44b44af6b4c565f83a0cbcde3ef0d8989
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-10 15:59:02 -0400

    test: assign with multiple spaces cases

--- test/liquid/combinators/tags/assign_test.exs
+++ test/liquid/combinators/tags/assign_test.exs
@@ -5,9 +5,26 @@ defmodule Liquid.Combinator.Tags.AssignTest do
   alias Liquid.NimbleParser, as: Parser
 
   test "assign" do
-    test_combinator("{% assign cart = 5 %}", &Parser.assign/1, [
-      {:assign, [variable_name: "cart", value: 5]},
-      ""
-    ])
+    tags = [
+      "{% assign cart = 5 %}",
+      "{%      assign     cart    =    5    %}",
+      "{%assign cart = 5%}",
+      "{% assign cart=5 %}",
+      "{%assign cart=5%}"
+    ]
+
+    Enum.each(tags, fn tag ->
+      test_combinator(tag, &Parser.assign/1, [
+        {:assign, [variable_name: "cart", value: 5]},
+        ""
+      ])
+    end)
+  end
+
+  test "incorrect variable assignation" do
+    test_combinator_error("{% assign cart@ = 5 %}", &Parser.assign/1)
+    test_combinator_error("{% assign cart. = 5 %}", &Parser.assign/1)
+    test_combinator_error("{% assign .cart = 5 %}", &Parser.assign/1)
+    test_combinator_error("{% assign cart? = 5 %}", &Parser.assign/1)
   end
 end
