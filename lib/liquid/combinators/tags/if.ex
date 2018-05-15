4177c7d9d381ce928a72392f4f1dfab206c6649f
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-05-14 20:57:18 -0400

    feat: If, elsif and else tag

--- lib/liquid/combinators/tags/if.ex
+++ lib/liquid/combinators/tags/if.ex
@@ -22,19 +22,29 @@ defmodule Liquid.Combinators.Tags.If do
     empty()
     |> parsec(:start_tag)
     |> ignore(string("if"))
-    |> times(choice([parsec(:conditions), parsec(:logical_conditions)]), min: 1)
+    |> concat(parsec(:ignore_whitespaces))
+    |> times(
+      choice([
+        parsec(:conditions),
+        parsec(:logical_conditions),
+        parsec(:boolean_value),
+        parsec(:token),
+        parsec(:logical_conditions_wo_math)
+      ]),
+      min: 1
+    )
     |> concat(parsec(:end_tag))
   end
 
   def conditions do
     parsec(:ignore_whitespaces)
-    |> concat(parsec(:object_value))
+    |> concat(parsec(:value_definition))
     |> concat(parsec(:ignore_whitespaces))
     |> concat(parsec(:math_operators))
     |> concat(parsec(:ignore_whitespaces))
     |> concat(parsec(:value_definition))
     |> concat(parsec(:ignore_whitespaces))
-    |>tag(:condition)
+    |> tag(:condition)
   end
 
   def logical_conditions do
@@ -42,10 +52,40 @@ defmodule Liquid.Combinators.Tags.If do
     |> concat(parsec(:conditions))
   end
 
-  @doc "Close if tag: {% endif %}"
-  def close_tag do
+  def logical_conditions_wo_math do
+    parsec(:logical_operators)
+    |> concat(parsec(:variable_name))
+  end
+
+  def elsif_tag do
     empty()
     |> parsec(:start_tag)
+    |> ignore(string("elsif"))
+    |> concat(parsec(:ignore_whitespaces))
+    |> times(
+      choice([parsec(:conditions), parsec(:logical_conditions), parsec(:boolean_value)]),
+      min: 1
+    )
+    |> concat(parsec(:end_tag))
+    |> tag(:elsif)
+  end
+
+  def else_tag do
+    # |> repeat_until(utf8_char([]), [string(General.codepoints().start_tag)])
+    # |> reduce({List, :to_string, []})
+    parsec(:ignore_whitespaces)
+    |> concat(parsec(:start_tag))
+    |> ignore(string("else"))
+    |> concat(parsec(:end_tag))
+    |> repeat_until(utf8_char([]), [string(General.codepoints().start_tag)])
+    |> tag(:else)
+
+    # |> repeat_until(utf8_char([]), [string(General.codepoints().start_tag)])
+  end
+
+  @doc "Close if tag: {% endif %}"
+  def close_tag do
+    parsec(:start_tag)
     |> ignore(string("endif"))
     |> concat(parsec(:end_tag))
   end
@@ -59,13 +99,24 @@ defmodule Liquid.Combinators.Tags.If do
   def if_content do
     empty()
     |> repeat_until(utf8_char([]), [string(General.codepoints().start_tag)])
-    |> choice([parsec(:close_tag_if), parsec(:not_close_tag_comment)])
+    |> reduce({List, :to_string, []})
+    |> times(
+      choice([
+        parsec(:elsif_tag),
+        parsec(:else_tag),
+        parsec(:close_tag_if),
+        parsec(:if),
+        parsec(:not_close_tag_if)
+      ]),
+      min: 1
+    )
   end
 
   def tag do
     empty()
-    #|> parsec(:open_tag_for)
+    |> parsec(:open_tag_if)
+    |> concat(parsec(:if_content))
+    |> tag(:if)
     |> optional(parsec(:__parse__))
-    #|> parsec(:open_tag_for)
   end
 end
