961949509951b84c150a0f7fa236da069097c130
fabianherrera@gmail.com <admnts9000!>
2018-05-16 16:25:02 -0400

    feat: nimble parser capture tag and test.

---
+++ test/liquid/combinators/tags/capture_test.exs
@@ -0,0 +1,27 @@
+defmodule Liquid.Combinator.Tags.CaptureTest do
+  use ExUnit.Case
+
+  import Liquid.Helpers
+  alias Liquid.NimbleParser, as: Parser
+
+  test "capture tag: parser basic structures" do
+    test_combinator(
+      "{% capture about_me %} I am {{ age }} and my favorite food is {{ favorite_food }}. {% endcapture %}",
+      &Parser.capture/1,
+      [
+        {:capture,
+          [
+            variable_name: "about_me",
+            capture_sentences: [
+              " I am ",
+              {:variable, [variable_name: "age"]},
+              " and my favorite food is ",
+              {:variable, [variable_name: "favorite_food"]},
+              ". "
+            ]
+          ]},
+        ""
+      ]
+    )
+  end
+end
