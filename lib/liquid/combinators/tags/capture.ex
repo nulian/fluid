c1b78c6fb6884489ef04a84bc65680bc6ce47550
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-05-25 10:37:46 -0400

    fix: variable repeat tag

--- lib/liquid/combinators/tags/capture.ex
+++ lib/liquid/combinators/tags/capture.ex
@@ -2,11 +2,11 @@ defmodule Liquid.Combinators.Tags.Capture do
   @moduledoc """
   Stores the result of a block into a variable without rendering it in place.
   ```
-  {% capture heading %}
-  Monkeys!
-  {% endcapture %}
-  ...
-  <h1>{{ heading }}</h1> <!-- then you can use the `heading` variable -->
+    {% capture heading %}
+      Monkeys!
+    {% endcapture %}
+    ...
+    <h1>{{ heading }}</h1> <!-- then you can use the `heading` variable -->
   ```
   Capture is useful for saving content for use later in your template, such as in a sidebar or footer.
   """
