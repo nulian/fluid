918f0c814b8fc4b0e7accc5fc712fcba7efad627
Piotr Szmielew <p.szmielew@ava.waw.pl>
2019-06-19 14:55:00 +0200

    Add types for Liquid.Context

--- lib/liquid/context.ex
+++ lib/liquid/context.ex
@@ -11,6 +11,20 @@ defmodule Liquid.Context do
             global_filter: nil,
             extra_tags: %{}
 
+  @type t :: %__MODULE__{
+          assigns: map(),
+          offsets: map(),
+          registers: map(),
+          presets: map(),
+          blocks: list(),
+          extended: boolean(),
+          continue: boolean(),
+          break: boolean(),
+          template: term(),
+          global_filter: term(),
+          extra_tags: map()
+        }
+
   def registers(context, key) do
     context.registers |> Map.get(key)
   end
